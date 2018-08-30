8193616c05bfe1fd5ccd0f59d2cbec67d40654b3
Vaskka <1139851358@qq.com>
2018-08-30 12:05:48 +0800

    change user sdk(register logic)

---
+++ User/src/com/vaskka/grand/user/response/TestRegisterResponseObject.java
@@ -0,0 +1,11 @@
+package com.vaskka.grand.user.response;
+
+/**
+ * @program: User
+ * @description: TestRegisterResponseObject 验证注册用户
+ * @author: Vaskka
+ * @create: 2018/8/30 上午11:39
+ **/
+
+public class TestRegisterResponseObject {
+}
