a18dd244d012d53a1a9c80170d8609b04cc12a12
Vaskka <1139851358@qq.com>
2018-10-18 23:57:24 +0800

    添加后台接口

--- backend/backend/chain/views.py
+++ backend/backend/chain/views.py
@@ -10,3 +10,76 @@ from utils.request import QueryRequest
 from utils.util import md5
 
 
+def user_try_register(request):
+    """
+    用户尝试注册
+    :param request:
+    :return:
+    """
+    pass
+
+
+def user_confirm_verify(request):
+    """
+    用户验证码验证
+    :param request:
+    :return:
+    """
+    pass
+
+
+def user_resend_code(request):
+    """
+    用户验证码重新发送
+    :param request:
+    :return:
+    """
+    pass
+
+
+def user_insert_face_token(request):
+    """
+    用户录入人脸信息
+    :param request:
+    :return:
+    """
+    pass
+
+
+def user_login(request):
+    """
+    用户登陆
+    :param request:
+    :return:
+    """
+
+
+    pass
+
+
+def user_logout(request):
+    """
+    用户登出
+    :param request:
+    :return:
+    """
+    pass
+
+
+def user_find_from_email_user_id(request):
+    """
+    利用邮箱查询user_id
+    :param request:
+    :return:
+    """
+
+    pass
+
+
+def transaction(request):
+    """
+    向用户发起转账
+    :param request:
+    :return:
+    """
+    pass
